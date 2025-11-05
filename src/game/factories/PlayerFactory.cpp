5188cc76aa7bfe6ac0a0e0ed18074a44a1255225
soerenlemke <61249756+soerenlemke@users.noreply.github.com>
2025-11-05 21:02:56 +0100

    Add components to PlayerFactory's CreatePlayer method

--- src/game/factories/PlayerFactory.cpp
+++ src/game/factories/PlayerFactory.cpp
@@ -10,8 +10,18 @@
 namespace Game {
     ECS::Entity PlayerFactory::CreatePlayer(ECS::EntityRegistry &registry, float x, float y, int spriteId) {
         const auto e = registry.CreateEntity();
+
         const auto tc = ECS::TransformComponent(x, y, 0, 0);
         registry.AddTransform(e, tc);
+
+        constexpr auto sc = ECS::SpriteComponent();
+        registry.AddSprite(e, sc);
+
+        constexpr auto cc = ECS::CollisionComponent();
+        registry.AddCollision(e, cc);
+
+        constexpr auto kc = ECS::KeyInputComponent();
+        registry.AddKeyInput(e, kc);
 
         return e;
     }
